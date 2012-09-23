727e0e448acaa9c59401d4962ce0d8a79f776811
Lori M Olson <lori@wndx.com>
2012-09-23 11:52:27 -0600

    Updated API

--- lib/field_trip_server/api.rb
+++ lib/field_trip_server/api.rb
@@ -1,15 +1,5 @@
 class FieldTripServer::API < Grape::API
   version 'v1', :using => :header
-
-  helpers do
-    def current_user
-      @current_user ||= User.authorize!(env)
-    end
-
-    def authenticate!
-      error!('401 Unauthorized', 401) unless current_user
-    end
-  end
   
   resource :schools do
     
@@ -30,10 +20,6 @@ class FieldTripServer::API < Grape::API
       user == "course" && password == "password"
     end
     
-    get :list do
-      ["Hello"]
-    end
-    
     post :create do
       puts "=== CREATE #{params.inspect}"
       school = School.where(:name => params[:school]).first
@@ -42,36 +28,5 @@ class FieldTripServer::API < Grape::API
     end
   end
   
-
-  # resource :statuses do
-  #   get :public_timeline do
-  #     Tweet.limit(20)
-  #   end
-  # 
-  #   get :home_timeline do
-  #     authenticate!
-  #     current_user.home_timeline
-  #   end
-  # 
-  #   get '/show/:id' do
-  #     Tweet.find(params[:id])
-  #   end
-  # 
-  #   post :update do
-  #     authenticate!
-  #     Tweet.create(
-  #       :user => current_user,
-  #       :text => params[:status]
-  #     )
-  #   end
-  # end
-  # 
-  # resource :account do
-  #   before { authenticate! }
-  # 
-  #   get '/private' do
-  #     "Congratulations, you found the secret!"
-  #   end
-  # end
 
 end
