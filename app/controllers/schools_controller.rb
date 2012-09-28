15172c602b8a7c74878812b299f6dc7004bda90b
Lori M Olson <lori@wndx.com>
2012-09-27 21:49:36 -0600

    Adds pagination

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -2,7 +2,7 @@ class SchoolsController < ApplicationController
   # GET /schools
   # GET /schools.json
   def index
-    @schools = School.all
+    @schools = School.paginate(:page => params[:page], :per_page => 10).order(:name)
 
     respond_to do |format|
       format.html # index.html.erb
