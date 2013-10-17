adccd759f01b983e8923a9aa5c4241c11d5a093f
Tim Breitkreutz <tim@generalthings.com>
2013-10-17 22:02:26 +0100

    Add seeds and data files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
+
+#
+# ROR4REAL
+# This is where you parse your CSV file, and stuff it into the database. You've already created the
+# migration for your model, and run it, right?
+#
+
+module ActiveModel
+  module MassAssignmentSecurity
+    class Sanitizer
+      def sanitize(attributes, authorizer)
+        attributes
+      end
+    end
+  end
+end
+
+
+file = "#{Rails.root}/db/CALGIS.SCHOOL_LOCATION.csv"
+CSV.foreach(file, :headers => true) do |row|
+  School.create(
+    :board => row[0],
+    :category => row[1],
+    :school_type => row[2],
+    :name => row[3],
+    :address => row[4],
+    :city => row[5],
+    :province => row[6],
+    :postal_code => row[7],
+    :grades => row[8],
+    :ecs => row[9] == 'Y'
+  )
+end    
