e96bcdf2c6728ebe7446dbde467a30d41d6be45f
Tim Breitkreutz <tim@generalthings.com>
2013-10-17 23:31:22 +0100

    post working

--- app/api/field_trip_server/api.rb
+++ app/api/field_trip_server/api.rb
@@ -9,4 +9,16 @@ class FieldTripServer::API < Grape::API
       School.all
     end
   end
+
+  resource :field_trips do
+    http_basic do |user, password|
+      user == "course" && password == "password"
+    end
+    
+    post :create do
+      school = School.where(:name => params[:school]).first
+      school.field_trips.create!(:name => params[:name]) unless school.nil?
+    end
+  end
+
 end
