1fc527b640ea036bad4ac7d4963243ecb0175ce3
Tim Breitkreutz <tim@generalthings.com>
2013-10-17 23:22:20 +0100

    add basic HTTP auth

--- app/api/field_trip_server/api.rb
+++ app/api/field_trip_server/api.rb
@@ -3,6 +3,10 @@ class FieldTripServer::API < Grape::API
   version 'v1', :using => :header, :vendor => 'rails4ios'
 
   resource :schools do
+    http_basic do |user, password|
+      user == "course" && password == "password"
+    end
+
     get :list do
       School.all
     end
