5958a74e7259e1e98c33f8c5222cb89a699b286d
Tim Breitkreutz <tim@generalthings.com>
2013-10-17 23:18:53 +0100

    view changes, run migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131017205717) do
+ActiveRecord::Schema.define(version: 20131017221452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "field_trips", force: true do |t|
+    t.string   "name"
+    t.integer  "school_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "field_trips", ["school_id"], name: "index_field_trips_on_school_id", using: :btree
 
   create_table "schools", force: true do |t|
     t.string   "board"
