1ee0752d0668962bfd9fab73b7a5559f1fea9ae3
Tim Breitkreutz <tim@generalthings.com>
2013-10-17 21:46:00 +0100

    Clean out Rails 3 stuff

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FieldTripServer::Application.config.secret_token = '3273ae18f6b69c84a27c160b9ca8882cabacd34fae11bb6fa167ec7bf4ae36b5b06256222beb85f7b3337ee53a2fb438d2e190886c053426fb2c29e3c96a42e9'
--- spec/factories/schools.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :school do
-    name "School of Rails"
-    province "AB"
-  end
-end
